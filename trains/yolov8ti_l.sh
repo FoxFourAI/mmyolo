9c082ff922734087780fe87c78b1ca41d6fc7974
Roman Vei <roman.vei@foxfour.ai>
2025-05-03 16:40:40 +0000

    Remove obsolete YOLOv8TI training scripts for models N, S, M, L, X, and the run_all_yolov8ti.sh script to streamline project structure and eliminate unused files.

--- trains/yolov8ti_l.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd $(dirname $(dirname $0))
-python tools/train.py configs/yolov8ti/yolov8ti_l.py --model-surgery 2
--- trains/yolov8ti_x.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd $(dirname $(dirname $0))
-python tools/train.py configs/yolov8ti/yolov8ti_x.py --model-surgery 2
